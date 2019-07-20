133239fada476a18e6103997dfa829e6cc44753a
Nebrassy <nebras30@gmail.com>
2019-07-20 23:31:57 +0300

    Build with TWRP
    Change-Id: I02af5d7622f09388d0990ad2be88657ce978782d

---
+++ twrp.mk
@@ -0,0 +1,41 @@
+ifeq ($(BUILD_TWRP),true)
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)/twrp
+
+# TWRP specific build flags
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/pane10-backlight/brightness"
+TW_MAX_BRIGHTNESS := 1023
+TW_DEFAULT_BRIGHTNESS := 420
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+AB_OTA_UPDATER := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT_DIR}/target/product/enchilada/system/lib64/android.hardware.boot@1.0.so
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TW_NO_SCREEN_BLANK := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_HAS_EDL_MODE := true
+
+# Encryption
+PLATFORM_SECURITY_PATCH := 2025-12-31
+TW_INCLUDE_CRYPTO := true
+
+# Extras
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_USE_LEDS_HAPTICS := true
+
+TW_USE_TOOLBOX := true
+endif
