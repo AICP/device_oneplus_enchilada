56f2c03405360463e1af8ae5579af3172c90c8d9
Julian Veit <Claymore1298@gmail.com>
2025-05-20 18:16:01 +0200

    enchilada: add maintainers
    Change-Id: Ief22641f21df195460d5398750aa75e8d9ecf175

--- aicp_enchilada.mk
+++ aicp_enchilada.mk
@@ -39,3 +39,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=OnePlus6 \
     SystemDevice=OnePlus6 \
     SystemName=OnePlus6
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Julian Veit (Claymore1297)"
